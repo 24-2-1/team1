99003bcaee3a38304372987d88f3ca71f22e59e4
naktan02 <jmgjmg102@gmail.com>
2024-12-02 01:13:46 +0900

    불필요한 파일 삭제

--- notifications.py
+++
@@ -1,7 +0,0 @@
-class Notifications:
-    def __init__(self, client_socket):
-        self.client_socket = client_socket
-
-    def check_notifications(self, user):
-        response = self.client_socket.send_request(f"check_notifications {user}")
-        print(f"{user}님의 알림: {response}")
